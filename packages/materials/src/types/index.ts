a052b57b5130dec2c8c9d70a44f2cf22a88b6225
一寸灰 <webyicunhui@outlook.com>
2026-01-12 16:24:28 +0800

    chore(deps): :arrow_up: update deps.

--- packages/materials/src/types/index.ts
+++ packages/materials/src/types/index.ts
@@ -152,7 +152,8 @@ export type LayoutScrollMode = 'wrapper' | 'content';
 
 /** Admin layout props */
 export interface AdminLayoutProps
-  extends AdminLayoutHeaderConfig,
+  extends
+    AdminLayoutHeaderConfig,
     AdminLayoutTabConfig,
     AdminLayoutSiderConfig,
     AdminLayoutContentConfig,
